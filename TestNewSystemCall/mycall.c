f71cfe933c0b135e36b3bd9c7740cf61de0d5954
Георгий <38286808+TeplCold@users.noreply.github.com>
2019-05-15 03:25:16 +0400

    Add files via upload

---
+++ TestNewSystemCall/mycall.c
@@ -0,0 +1,17 @@
+#include "exp.h"
+
+NTSTATUS NtMyNewSystemCall(int Count)
+{
+	int i;
+
+	static long callCount = 1;
+
+	for (i = 0; i < Count; i++)
+	{
+		DbgPrint("WRK !!!NtMyNewSystemCall: %d\n", callCount);
+	}
+
+	callCount++;
+
+	return STATUS_SUCCESS;
+}
